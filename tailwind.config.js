7bbdecdac899c42b9ae6a00aa5e55b575e536697
Guga Akopashvili <guga.akopashvili1711@gmail.com>
2025-01-19 12:18:02 +0400

    core/structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        natoSans: ["Nato-Sans", "sans-serif"],
+        natoSans: ['Nato-Sans', 'sans-serif'],
       },
       fontSize: {
-        "title-sm": "32px",
-        subtitle: "22px",
-        body: "16px",
-        "body-sm": "12px",
-        "body-md": "14px",
-        small: "12px",
-        button: "16px",
+        'title-sm': '32px',
+        subtitle: '22px',
+        body: '16px',
+        'body-sm': '12px',
+        'body-md': '14px',
+        small: '12px',
+        button: '16px',
       },
     },
   },
