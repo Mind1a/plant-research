5f35da1be91e2bc758851ffecb32ad6e1695402c
giojano1 <giorgi.janelidze64@gmail.com>
2025-01-24 03:52:05 +0400

    feature/footer
    adding footer component to layout and both tablet and mobile versions

--- src/presenter/assets/index.ts
+++ src/presenter/assets/index.ts
@@ -2,4 +2,6 @@ import logo from '/assets/svg/logo.svg';
 import fullLogo from '/assets/svg/logoFull.svg';
 import searchIcon from '/assets/icons/search.svg';
 import cancelIcon from '/assets/icons/cancel.svg';
-export { logo, fullLogo, searchIcon, cancelIcon };
+import unilabLogo from '/assets/svg/unilabLogo.svg';
+import iliauniLogo from '/assets/svg/iliauniLogo.svg';
+export { logo, fullLogo, searchIcon, cancelIcon, unilabLogo, iliauniLogo };
