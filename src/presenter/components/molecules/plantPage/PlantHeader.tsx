d7ae4212c9681a34e3213294b153d8965a7fa86b
giojano1 <giorgi.janelidze64@gmail.com>
2025-02-26 18:36:25 +0400

    feature/plant-header

--- src/presenter/components/molecules/plantPage/PlantHeader.tsx
+++ src/presenter/components/molecules/plantPage/PlantHeader.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import { useNavigate } from 'react-router';
+import Button from '../../atoms/buttons/Button';
 
 export default function PlantHeader() {
-  return <div>PlantHeader</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex items-center justify-between">
+      <Button onclick={() => navigate('/sarkvevi')} label="სარკვევი" />
+      <Button
+        onclick={() => navigate('/families')}
+        variant="tertiary"
+        label="ოჯახები"
+      />
+    </div>
+  );
 }
