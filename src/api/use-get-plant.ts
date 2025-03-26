6b26cc96d13f0afb282640ac2e829b7692981e3a
giojano1 <giorgi.janelidze64@gmail.com>
2025-03-27 01:38:37 +0400

    feature/get-plant-call-update

--- src/api/use-get-plant.ts
+++ src/api/use-get-plant.ts
@@ -3,13 +3,14 @@ import API from './api';
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { Plant } from '../types';
 
-const fetchPlantById = async (id: string): Promise<Plant | null> => {
+const fetchPlantById = async (id?: string): Promise<Plant | null> => {
+  if (!id) return null;
   try {
-    const res = await API.get<Plant[]>(`/plant/${id}`);
-
-    return res.data.length > 0 ? res.data[0] : null;
+    const { data } = await API.get<Plant>(`/plant/${id}`);
+    console.log(data);
+    return data || null;
   } catch (error) {
-    console.error('Error fetching plant by id', error);
+    console.error(`Error fetching plant with ID ${id}:`, error);
     throw error;
   }
 };
@@ -21,5 +22,6 @@ export const useGetPlant = (): UseQueryResult<Plant | null, Error> => {
     queryFn: () => fetchPlantById(id as string),
     enabled: !!id,
     staleTime: 1000 * 60 * 5,
+    retry: 1,
   });
 };
