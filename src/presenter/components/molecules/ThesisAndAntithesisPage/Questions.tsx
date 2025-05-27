55f828e4f1facb891efcfaf32251cf8e8505b201
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-05-28 00:50:35 +0400

    update/ PreviousQuestion-RestartQuiz
    Implemented functionality for navigating to the previous question and restarting the quiz in the quiz component.

--- src/presenter/components/molecules/ThesisAndAntithesisPage/Questions.tsx
+++ src/presenter/components/molecules/ThesisAndAntithesisPage/Questions.tsx
@@ -2,9 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchquestions } from '../../../../api/use-get-question';
 import { Question } from '../../../../types';
 import { useState } from 'react';
+import Loader from '../../atoms/loader/Loader';
+import Button from '../../atoms/buttons/Button';
 
 const Questions = () => {
   const [questionList, setQuestionList] = useState<number[]>([1]);
+
   const lastElement = questionList[questionList.length - 1];
 
   const { isLoading, error, data } = useQuery<Question[]>({
@@ -12,25 +15,53 @@ const Questions = () => {
     queryFn: () => fetchquestions(lastElement),
   });
 
-  if (isLoading) return <p>Loading ...</p>;
+  const backButton = () => {
+    setQuestionList((prev) => {
+      if (prev.length > 1) {
+        return prev.slice(0, -1);
+      }
+      return prev;
+    });
+  };
+
+  const resetButton = () => {
+    setQuestionList([1]);
+  };
+
+  if (isLoading) return <Loader />;
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div className="m-auto mt-[16px] flex flex-wrap justify-center gap-[30px] max-750:mt-[36px] max-450:mt-[24px]">
-      {data?.map((res, index) => (
-        <div
-          onClick={() => {
-            setQuestionList((prev) => [...prev, res.next_question_id]);
-          }}
-          className="questionCardShadow flex min-h-[426px] w-full max-w-[570px] cursor-pointer flex-col gap-[24px] rounded-[10px] border border-[#DFE4EA] p-[24px] max-750:min-h-[243px] max-750:gap-[12px] max-450:p-[16px]"
-          key={index}
-        >
-          <div className="min-h-[150px] w-full bg-[#00000004] max-450:min-h-[75px]"></div>
-          <div className="max-h-[230px] overflow-y-auto">
-            <p className="text-[12px] max-750:text-[14px]">{res.question}</p>
+    <div>
+      <div className="flex items-center justify-between">
+        <Button
+          onclick={backButton}
+          clasname="px-[24px] max-450:px-[44px] tryAgainButtonShadow max-450:text-[14px] max-750:text-[13px] max-750:max-h-[40px] max-750:px-[20px] max-750:text-[13px]"
+          label="უკან"
+        />
+        <Button
+          onclick={resetButton}
+          clasname="max-750:px-[20px] tryAgainButtonShadow max-750:text-[13px] max-750:max-h-[40px] max-450:px-[44px] max-450:text-[12px]"
+          variant="quaternary"
+          label="თავიდან დაწყება"
+        />
+      </div>
+      <div className="m-auto mt-[16px] flex flex-wrap justify-center gap-[30px] max-750:mt-[36px] max-450:mt-[24px]">
+        {data?.map((res, index) => (
+          <div
+            onClick={() => {
+              setQuestionList((prev) => [...prev, res.next_question_id]);
+            }}
+            className="questionCardShadow flex min-h-[426px] w-full max-w-[570px] cursor-pointer flex-col gap-[24px] rounded-[10px] border border-[#DFE4EA] p-[24px] max-750:min-h-[243px] max-750:gap-[12px] max-450:p-[16px]"
+            key={index}
+          >
+            <div className="min-h-[150px] w-full bg-[#00000004] max-450:min-h-[75px]"></div>
+            <div className="max-h-[230px] overflow-y-auto">
+              <p className="text-[12px] max-750:text-[14px]">{res.question}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
--- src/presenter/views/ThesisAndAntithesis.tsx
+++ src/presenter/views/ThesisAndAntithesis.tsx
@@ -1,11 +1,9 @@
 import PageWrapper from '../components/atoms/PageWrapper';
-import QuestionHeader from '../components/molecules/ThesisAndAntithesisPage/QuestionHeader';
 import Questions from '../components/molecules/ThesisAndAntithesisPage/Questions';
 
 export default function ThesisAndAntithesis() {
   return (
     <PageWrapper classname="my-[72px] max-1000:my-[42px] max-500:my-[27px] p-0">
-      <QuestionHeader />
       <Questions />
     </PageWrapper>
   );
