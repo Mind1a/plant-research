3cd6d505f42f3f9fae4227667f27c77ae529bd36
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-06-19 03:14:22 +0400

    feature/search error

---
+++ src/presenter/components/NoSearchResults/noSearchResults.tsx
@@ -0,0 +1,9 @@
+const NoSearchResults = () => {
+  return (
+    <div className="flex h-full w-full flex-1 items-center justify-center">
+      <p className="flex">მცენარის ოჯახი ვერ მოიძებნა</p>
+    </div>
+  );
+};
+
+export default NoSearchResults;
